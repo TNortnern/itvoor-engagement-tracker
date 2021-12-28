ab1aead57fcebc996c3c4b3ad388ad1d548f024d
unknown <3kompas.kompas3@gmail.com>
2021-12-28 23:48:24 +0000

    remove favourite tracking

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   collections: [
-    { name: 'organizations', actions: ['impressions', 'clicks', 'favourites'] },
+    { name: 'organizations', actions: ['impressions', 'clicks'] },
     { name: 'jobs', actions: ['impressions', 'clicks', 'applications'] },
     { name: 'podcasts', actions: ['impressions', 'clicks', 'plays'] },
     { name: 'events', actions: ['impressions', 'clicks', 'link-clicks'] },
--- index.js
+++ index.js
@@ -17,4 +17,4 @@ collections.forEach(col => {
   })
 })
 
-app.listen(3000, () => console.log('Listening port 3000...'))
+app.listen(process.env.PORT || 3000, () => console.log('Listening port 3000...'))
